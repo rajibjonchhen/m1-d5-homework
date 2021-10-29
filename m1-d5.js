5e2769f6546b5e3588cab978610023e48332da22
rajibjonchhen <51174335+rajibjonchhen@users.noreply.github.com>
2021-10-29 19:58:09 +0100

    m1-d5-with-extra
    with extra exercises

--- m1-d5.js
+++ m1-d5.js
@@ -167,17 +167,155 @@ headingTitle(8)
 /* EXERCISE 8
  Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
 */
-
 /* WRITE YOUR ANSWER HERE */
+let word = []
+const upperFirst = function (string8){
+   let afterSplitArray = string8.split(" ")
+    
+    for(let i = 0; i < afterSplitArray.length; i++){   
+        
+        let temp = afterSplitArray[i].split("")
+            temp[0] = temp[0].toUpperCase()
+            temp= temp.join("")
+            word.push(temp)
+        }
+        word = word.join(" ")
+        console.log(word)       
+    }
+
+upperFirst("is this really happening, i can't believe it")
+
+
+
+headingTitle(9)
+
 
 /* EXERCISE 9
  Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
 */
-
 /* WRITE YOUR ANSWER HERE */
+
+word = []
+const cutString = function (string8){
+        let temp = string8.split("")
+            temp[0] = [""]
+            temp[string8.length -1] = [""]
+        word = temp.join("")
+        console.log(word)       
+    }
+
+cutString("is this really happening, i can't believe it")
+
+cutString("jonchhen")
+
+
+headingTitle(10)
 
 /* EXERCISE 10
  Write a function called "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
+*/
+/* WRITE YOUR ANSWER HERE */
+let newArray = [];
+const giveMeRandom = function(n){
+    for(let i = 0; i <  n; i++){
+            newArray[i] = parseInt((Math.random() * 10))
+          }
+          return(newArray)
+    }
+
+giveMeRandom(10)
+console.log(newArray)
+
+/* WHEN YOU ARE FINISHED
+ Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today's Eduflow.
+*/
+
+
+
+// Additional assignments for Day 5
+headingTitle("Extra 1")
+/* EXTRA 1
+ Write a function called "checkArray" which receives an array of random numbers (created with giveMeRandom) and prints, for each item, whether it's bigger than 5 or not.
+ The function should return the sum of just the numbers bigger than 5.
+*/
+/* WRITE YOUR ANSWER HERE */
+let arrayBiggerThan5 = []
+const checkArray = function(giveMeRandom(n)){
+    let t = n
+      arrayBiggerThan5.push(giveMeRandom(t))
+console.log(arrayBiggerThan5)
+}
+
+checkArray(5)
+
+headingTitle("Extra 2")
+/* EXTRA 2
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "shoppingCartTotal" which calculates the total due to the shop.
+*/
+/* WRITE YOUR ANSWER HERE */
+
+
+
+/* EXTRA 3
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "addToShoppingCart" which receives a new object, adds it to shoppingCart and returns the total number of items in the shoppingCart.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 4
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "maxShoppingCart" which receives the shoppingCart array and returns the most expensive item in it.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 5
+ In your eCommerce you have an array of objects called shoppingCart. Each one of these objects has a price, a name, an id and the quantity to be shipped.
+ Create a function called "latestShoppingCart" which receives the shoppingCart array and returns the last item.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 6
+ Create a function called "loopUntil" which receives an integer x between 0 and 9 as a parameter.
+ The function loops and prints a random number between 0 and 9 until the random number is bigger than x for three times in a row.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 7
+ Write a function called "average" which receives an array and returns the average numerical value. The function automatically skips non-numeric entries in the array.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 8
+ Write a function called "longest" to find the longest string from a given array of strings.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 9
+ Write a function to create a very simple anti spam filter for your mailbox. The function takes a string emailContent, and returns a boolean.
+ The function should return true if the emailContent string does not contain the words SPAM or SCAM.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 10
+ Write a function that receives a date as a parameter and calculates the number of days passed since the given date.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 11
+ Write a function called "matrixGenerator" which receives two integers, x and y, as parameters.
+ The result should be a matrix of x times y with, as value, the index of the position.
+ Ex.: x = 3, y = 2
+ ["00","01","02"
+ "10","11","12"]
 */
 
 /* WRITE YOUR ANSWER HERE */
